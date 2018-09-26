62a68f9e309b0f7587ba08879cc53184e4f24250
objectisundefined <2535612040@qq.com>
2018-09-26 15:13:08 +0800

    feature: traverse cycle graph

---
+++ src/test/scala/fpinscala/datastructures/GraphSpec.scala
@@ -0,0 +1,64 @@
+package fpinscala.datastructures.graphs
+
+import org.scalatest.{ WordSpec, Matchers }
+
+class ListSpec extends WordSpec with Matchers {
+  import fpinscala.datastructures.graphs.Graph._
+
+  val graph = List(
+    ("m", "n"), ("m", "o"), ("m", "p"),
+    ("o", "r"),
+    ("n", "q"),
+    ("p", "q"),
+    ("q", "r"), ("q", "s")
+  )
+
+  "succSet" should {
+    "works" in {
+      succSet("m", graph) shouldEqual List("n", "o", "p")
+    }
+  }
+
+  "depthFirst" should {
+    "works" in {
+      depthFirst("m", graph) shouldEqual List("m", "n", "q", "r", "s", "o", "p")
+    }
+  }
+
+  "topsort" should {
+    "works" in {
+      topsort(graph) shouldEqual List("m", "n", "o", "p", "q", "r", "s")
+    }
+  }
+
+  val grwork = List(
+    ("getup","shower"),
+    ("shower", "breakfast"),
+
+    ("breakfast","dress"),
+    ("dress","office"),
+    ("office", "dinner"),
+
+    ("breakfast","leisurely_lunch"),
+    ("leisurely_lunch", "movie"),
+    ("movie", "breakfast")
+  )
+
+  "topsortWithCycle" should {
+    "throw Exception on cycle graph" in  {
+      intercept[java.lang.RuntimeException] {
+        topsortWithCycle(grwork)
+      }
+    }
+  }
+
+  "topsortPrintCycle" should {
+    "works" in {
+      topsortPrintCycle(grwork) shouldEqual (
+        List("getup", "shower", "breakfast", "dress", "office", "dinner", "leisurely_lunch", "movie"),
+        List("breakfast")
+      )
+    }
+  }
+
+}
