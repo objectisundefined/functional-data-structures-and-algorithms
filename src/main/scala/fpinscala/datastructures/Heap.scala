e4948ed8ef8d72e80960969b0eeaf2d8b0ee172d
objectisundefined <2535612040@qq.com>
2018-10-01 21:33:20 +0800

    feature: refactor and add test for LeftistHeap

--- src/main/scala/fpinscala/datastructures/Heap.scala
+++ src/main/scala/fpinscala/datastructures/Heap.scala
@@ -1,53 +1,46 @@
 package fpinscala.datastructures.heaps
 
-object LeftistHeap extends App {
-  sealed abstract class TreeNode {
-    def rank: Int
+object LeftistHeap {
+  sealed trait Tree {
+    val rank: Int
   }
 
-  case class Node(rank: Int, v: Int, left: TreeNode, right: TreeNode)
-    extends TreeNode
-
-  case object Leaf extends TreeNode {
+  case object Leaf extends Tree {
     override val rank = 0
   }
 
-  def makeNode(v: Int, left: TreeNode, right: TreeNode): Node =
+  case class Node(rank: Int, value: Int, left: Tree, right: Tree) extends Tree
+
+  def makeNode(v: Int, left: Tree, right: Tree): Node = {
     if (left.rank >= right.rank) Node(left.rank + 1, v, left, right)
     else Node(right.rank + 1, v, right, left)
-
-  def merge(node1: TreeNode, node2: TreeNode): TreeNode = (node1, node2) match {
-    case (node1, Leaf) => node1
-    case (Leaf, node2) => node2
-    case (Node(_, x, a1, b1), Node(_, y, a2, b2)) => {
-      if (x < y) makeNode(x, a1, merge(b1, node2))
-      else makeNode(y, a2, merge(node1, b2))
-    }
   }
 
-  def insert(v: Int, h: TreeNode): TreeNode = {
-    val singleTree = makeNode(v, Leaf, Leaf)
-    merge(singleTree, h)
+  def merge(node1: Tree, node2: Tree): Tree = (node1, node2) match {
+    case (node, Leaf) => node
+    case (Leaf, node) => node
+    case (Node(_, v1, l1, r1), Node(_, v2, l2, r2)) =>
+      if (v1 < v2) makeNode(v1, l1, merge(r1, node2))
+      else makeNode(v2, l2, merge(node1, r2))
   }
 
-  def min (h:TreeNode): Int = h match {
-    case Leaf => throw new IllegalArgumentException("Empty tree")
-    case (Node(_, x, _, _)) => x
+  def insert(v: Int, h: Tree): Tree = merge(makeNode(v, Leaf, Leaf), h)
+
+  def min (h:Tree): Int = h match {
+    case Leaf => sys.error("min on leaf")
+    case Node(_, v, _, _) => v
   }
 
-  def pop(h:TreeNode): (Int, TreeNode) = h match {
-    case Leaf => throw new IllegalArgumentException("Empty tree")
-    case (Node(_, x, a, b)) => (x, merge(a, b))
+  def pop(h:Tree): (Int, Tree) = h match {
+    case Leaf => sys.error("pop on leaf")
+    case Node(_, v, l, r) => (v, merge(l, r))
   }
 
-  def empty: TreeNode = Leaf
-
-  val q = List(1, 2, 3, 4, 5).foldLeft(empty)((a, b) => insert(b, a))
-
-  val (one, q1) = pop(q)
-  println(one)
-
-  val (two, q2) = pop(q1)
-  println(two)
+  val toList: Tree => List[Int] = h => h match {
+    case Leaf => List[Int]()
+    case _ =>
+      val (v, n) = pop(h)
+      v :: toList(n)
+  }
 
 }
---
+++ src/test/scala/fpinscala/datastructures/Heap.scala
@@ -0,0 +1,57 @@
+package fpinscala.datastructures.heaps
+
+import org.scalatest.{ WordSpec, Matchers }
+
+class LeftistHeapSpec extends WordSpec with Matchers {
+  import fpinscala.datastructures.heaps.LeftistHeap._
+
+  val emptyH: Tree = Leaf
+  val toHeap = (l: List[Int]) => l.foldLeft(emptyH)((a, b) => insert(b, a))
+
+  "intert" should {
+    "works" in {
+      toHeap((1 to 8).toList)
+    }
+  }
+
+  "merge" should {
+    "works" in {
+      merge(toHeap(List(1, 2)), toHeap(List(3, 4))) shouldEqual toHeap(List(1, 2, 3, 4))
+    }
+  }
+
+  "min" should {
+    "throw Exception on leaf" in  {
+      intercept[java.lang.RuntimeException] {
+        min(emptyH)
+      }
+    }
+    "works" in {
+      min(toHeap(List(1, 2, 3, 4, 5))) shouldEqual 1
+    }
+  }
+
+  "pop" should {
+    "throw Exception on leaf" in  {
+      intercept[java.lang.RuntimeException] {
+        pop(emptyH)
+      }
+    }
+    "works" in {
+      val h = toHeap(List(1, 2))
+
+      val (one, h1) = pop(h)
+      one shouldEqual 1
+
+      val (two, h2) = pop(h1)
+      two shouldEqual 2
+    }
+  }
+
+  "toList" should {
+    "works" in {
+      toList(emptyH) shouldEqual List[Int]()
+      toList(toHeap(List(1, 2, 3, 4))) shouldEqual List(1, 2, 3, 4)
+    }
+  }
+}
